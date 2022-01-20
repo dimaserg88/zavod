b6b80b9c720dd18754cea7fadb9f38616da1f667
dimaserg88 <dimaserg88@yndex.ru>
2022-01-21 02:49:25 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+const app = {
+    data() {
+        return {
+            message: 'Привет, Vue.js!',
+            navMobile: false,
+            popupCallback: false,
+            popupCalculator: false
+        }
+    },
+    methods: {
+        openMobileNav() {
+            this.navMobile = !this.navMobile
+            document.querySelector("html").classList.toggle("hidden");
+        },
+        setAppColor(color) {
+            localStorage.setItem('appColor', color)
+            document.documentElement.style.setProperty('--main-yellow-color', color);
+        },
+        openPopupCallback() {
+            this.popupCallback = !this.popupCallback
+        },
+        openPopupCalculator() {
+            this.popupCalculator = !this.popupCalculator
+        }
+    },
+    mounted() {
+        const appColor = localStorage.getItem('appColor')
+        if (appColor) {
+            document.documentElement.style.setProperty('--main-yellow-color', appColor);
+        } else {
+            localStorage.setItem('appColor', '#FDDA07')
+            document.documentElement.style.setProperty('--main-yellow-color', '#FDDA07');
+        }
+        document.querySelector("#app").classList.add("show");
+    }
+}
+
+Vue.createApp(app).mount('#app')
